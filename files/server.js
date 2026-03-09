828ff01a7a42a44750fc289e5c54be98589e268a
Etsub <sarinamehari@gmail.com>
2026-03-09 11:13:42 -0700

    using next

--- files/server.js
+++ files/server.js
@@ -18,9 +18,20 @@ app.get('/old-page.html',(req,res)=>{
     res.redirect(301,'/new-page.html')
 })
 
+app.get('/hello.html',(req,res,next)=>{
+    console.log('attempted to load hello.html')
+    next()
+},(req,res) =>{
+    res.send("hello world")
+})
+
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, '..', 'views', '404.html'));
 });
+
+
+
+app.get('/ ')
 app
 
 
