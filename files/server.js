a1ed6b15b07651eabd9de55054e7a65ec6aa5ea8
Etsub <sarinamehari@gmail.com>
2026-03-10 02:23:52 -0700

    adding logEvent to my custom middle ware

--- files/server.js
+++ files/server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
-
+const logEvents = require('../middleware/logEvents.js')
+ 
+//custom middle ware
 app.use((req,res,next) => {
-    console.log(`${req.method} ${req.path}`)
+    logEvents(`${req.method} ${req.headers.origin}\t ${req.url}`,'reqLog.txt')
+    console.log(`${req.method} ${req.url}`)
     next()
 })
 
