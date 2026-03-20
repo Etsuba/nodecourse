2b2ee70314efb660358ae4c6698a4c052194c741
Etsub <sarinamehari@gmail.com>
2026-03-20 07:01:45 -0700

    3rd middle ware ,listing origins to be allowed by cord

--- files/server.js
+++ files/server.js
@@ -3,9 +3,26 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
 const {logger} = require('../middleware/logEvents.js')
- 
+const cors = require('cors')
+
+
 //custom middle
 app.use(logger)
+
+//3rd middleware
+const whitelist = []
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whilelist.indexOf(origin) !== -1 ){
+            callback(null, true)
+        } else {
+            callback(new Error('not allowed by CORS'))
+        }
+    },optionsSucessStatus: 200
+}
+app.use(cors(corsOptions))
+
+
 
 //built in middel wares
 app.use(express.urlencoded({extended: false}))
