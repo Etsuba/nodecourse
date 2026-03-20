93fdb34c486afbd43a368fb65d994b240d934d47
Etsub <sarinamehari@gmail.com>
2026-03-20 07:23:57 -0700

    custom error handling

--- files/server.js
+++ files/server.js
@@ -1,87 +1,75 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const PORT = process.env.PORT || 3500
-const {logger} = require('../middleware/logEvents.js')
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+const { logger } = require('../middleware/logEvents.js');
+const cors = require('cors');
 
+// custom middleware
+app.use(logger);
 
-//custom middle
-app.use(logger)
-
-//3rd middleware
-const whitelist = []
+// CORS setup
+const whitelist = ['http://localhost:3000', 'https://myapp.com'];
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (whilelist.indexOf(origin) !== -1 ){
-            callback(null, true)
-        } else {
-            callback(new Error('not allowed by CORS'))
-        }
-    },optionsSucessStatus: 200
-}
-app.use(cors(corsOptions))
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
+// built-in middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
+// routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+});
 
-//built in middel wares
-app.use(express.urlencoded({extended: false}))
+app.get('/new-page.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'new-page.html'));
+});
 
-app.use(express.json())
+app.get('/old-page.html', (req, res) => {
+  res.redirect(301, '/new-page.html');
+});
 
-app.use(express.static(path.join(__dirname,'..','/public')))
-app.get('/',(req,res)=>{
-    // res.sendFile(path.join(__dirname,"..","./views/index.html"))
-    res.sendFile(path.join(__dirname,"..","./views/index.html"))
-})
+app.get('/hello.html', (req, res, next) => {
+  console.log('attempted to load hello.html');
+  next();
+}, (req, res) => {
+  res.send("hello world");
+});
 
+const one = (req, res, next) => {
+  console.log('one');
+  next();
+};
 
+const two = (req, res) => {
+  console.log('two');
+  res.send('finished');
+};
 
-app.get('/new-page.html',(req,res)=>{
-    // res.sendFile(path.join(__dirname,"..","./views/index.html"))
-    res.sendFile(path.join(__dirname,"..","./views/new-page.html"))
-})
+app.get('/chain.html', [one, two]);
 
-app.get('/old-page.html',(req,res)=>{
-    // res.sendFile(path.join(__dirname,"..","./views/index.html"))
-    res.redirect(301,'/new-page.html')
-})
-
-app.get('/hello.html',(req,res,next)=>{
-    console.log('attempted to load hello.html')
-    next()
-},(req,res) =>{
-    res.send("hello world")
-})
-
-const one = (req, res, next)=>{
-    console.log('one')
-    next()
-}
-
-
-const two = (req, res, next)=>{
-    console.log('two')
-    res.send('finished')
-}
-
-app.get('/chain.html',[one,two])
-
+// 404 handler
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, '..', 'views', '404.html'));
 });
 
+// error handler
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send(err.message);
+});
 
-
-
-
-
-app.get('/ ')
-app
-
-
-
-
-app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
