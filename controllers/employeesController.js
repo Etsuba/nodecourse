4a44f84953fbe29d77ccead369c3b3b3311af017
Etsub <sarinamehari@gmail.com>
2026-03-27 11:51:01 -0700

    fiishing the update

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -44,7 +44,8 @@ const updateEmployee =  (req, res) => {
     if (req.body.lastname) employee.lastname = req.body.lastname;
     const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
     const unsortedArray = [...filteredArray, employee];
-   
+    data.setEmployees(unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
+    res.json(data.employees);
 }
 
     
