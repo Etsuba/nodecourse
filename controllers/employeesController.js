6bef7080c02e472119b38c57d69dd3f5e8a7cb59
Etsub <sarinamehari@gmail.com>
2026-03-27 11:52:02 -0700

    deleting

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -50,13 +50,13 @@ const updateEmployee =  (req, res) => {
 
     
 
-const deleteEmployee = (req,res)=>{
-        res.json({"id":req.body.id})
-      }
-
-const getEmployee = (req,res)=>{
-                res.json({"id": req.params.id})
-             }
+const deleteEmployee =  (req, res) => {
+    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+    if (!employee) {
+        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` });
+    }
+   
+}
 
 
 module.exports = {
