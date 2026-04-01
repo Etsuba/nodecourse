4248d8cad8155cdca73485e0301fa657c436e82e
Etsub <sarinamehari@gmail.com>
2026-04-01 09:15:59 -0700

    authenticaating 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,3 +5,25 @@ const usersDB = {
     }
 
 }
+
+const bcrypt = require("bcrypt")
+
+const handleLogin = async (req,res) =>{
+    const {user,pwd} = req.body
+    if (!user || !pwd) return res.status(400).json({'message':"username and password are required"})
+
+        const foundUser = usersDB.users.find(person => person.username === user)
+        if(foundUser) return res.sendStatus(401);//unauthorized
+
+        //evaluate password 
+         const match = await bcrypt.compare(pwd,foundUser.password)
+         if (match) {
+                 // create jwt,
+            res.json("success" `user ${user} is logged in`)
+         }else {
+            res.sendStatus(401)
+         }
+
+        }
+
+module.exports = {handleLogin}
