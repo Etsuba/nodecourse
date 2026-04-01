09ed3bb7154d0406794fe125e5e53f476a5f161a
Etsub <sarinamehari@gmail.com>
2026-04-01 01:28:16 -0700

    handlingnewusers

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -8,4 +8,28 @@ const usersDB = {
 
 const fsPromises = require('fs').promises;
 const path = require("path")
- 
+const bcrypt = require("bcrypt")
+
+const handleNewUser = async (req,res) =>{
+    const {user,pwd} = req.body
+    if (!user || !pwd) return res.status(400).json({'message':"username and password are required"})
+
+    //check for duplicate user name in db
+
+    const duplicate = usersDB.users.find(person => person.username === user)
+    if(duplicate) return res.sendStatus(409);//conflict
+
+    try {
+      //encrypt the passs
+
+
+
+
+    } catch (err){
+        res.status(500).json({'message':err.message})
+    }
+
+}
+
+
+
