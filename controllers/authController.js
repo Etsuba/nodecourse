ddb5ff1c78db4720ac997913996f1436e975078e
Etsub <sarinamehari@gmail.com>
2026-04-15 03:26:27 -0700

    correcing the jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,7 +46,7 @@ const handleLogin = async (req,res) =>{
                  )
         
             res.cookie('jwt', refreshToken,{ httpOnly:true ,maxAge:24*60*60*1000})
-            res.json("success" `user ${user} is logged in`)
+            res.json(accessToken)
          }else {
             res.sendStatus(401)
          }
