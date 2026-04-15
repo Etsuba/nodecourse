c3458e1e94a306c9faf83076e80e3661b4d891ef
Etsub <sarinamehari@gmail.com>
2026-04-15 03:03:21 -0700

    including jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,10 @@ const usersDB = {
 }
 
 const bcrypt = require("bcrypt")
+require('dotenv').config()
+const fsPromises =require('fs').promises
+const path = require('path')
+const jwt = require('jsonwebtoken')
 
 const handleLogin = async (req,res) =>{
     const {user,pwd} = req.body
@@ -19,6 +23,16 @@ const handleLogin = async (req,res) =>{
          const match = await bcrypt.compare(pwd,foundUser.password)
          if (match) {
                  // create jwt,
+                 const accessToken = jwt.sign(
+                    {"username": foundUser.username},
+                    process.env.ACCESS_TOKE_SECRET,
+                    {expiresIn: '30s'}
+                 )
+                 const refreshToken = jwt.sign(
+                    {"username": foundUser.username},
+                    process.env.REFRESH_TOKE_SECRET,
+                    {expiresIn: '1d'}
+                 )
             res.json("success" `user ${user} is logged in`)
          }else {
             res.sendStatus(401)
