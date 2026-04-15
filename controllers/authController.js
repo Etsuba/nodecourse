23168356a3e8d728eb5937b5e3e4c141e031d27e
Etsub <sarinamehari@gmail.com>
2026-04-15 03:23:00 -0700

    refresh token

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,7 +35,7 @@ const handleLogin = async (req,res) =>{
                  )
 
                  // to conect it to db so wjen someone logs out it will erase or invalidate the refresh token
-                 //saving refrshToken with the cirrent user so we can eaasily erse the refresh token if they log outgit 
+                 //saving refrshToken with the cirrent user so we can eaasily erse the refresh token if they log out before day 1 ends 
                  const otherUser = usersDB.users.filter( person=> person.username !== foundUser.username)
                  const currentUser = {...foundUser , refreshToken}
                  usersDB.setUsers([...otherUser, currentUser])
@@ -44,6 +44,8 @@ const handleLogin = async (req,res) =>{
                     JSON.stringify(usersDB.users)
 
                  )
+        
+            res.cookie('jwt', refreshToken,{ httpOnly:true ,maxAge:24*60*60*1000})
             res.json("success" `user ${user} is logged in`)
          }else {
             res.sendStatus(401)
